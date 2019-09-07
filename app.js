3cbfb4e535cbaa92702050317182f97bdd391bcd
ajitabhsidhan <ajitabhsidhan@nbnco.com.au>
2019-09-08 08:51:40 +1000

    modified links

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var sassMiddleware = require('node-sass-middleware');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var ihtRouter = require('./routes/iht');
 
 var app = express();
 
@@ -35,6 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/iht', ihtRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
